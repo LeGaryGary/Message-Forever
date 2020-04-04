0a028eebf3aac2f494f805358631667b4c5736ef
Gary Gary <gary@garygary.uk>
2020-04-04 06:14:57 +0100

    WIP

---
+++ src/components/Messages.svelte
@@ -0,0 +1,15 @@
+<script>
+  import NavBar from './NavBar.svelte';
+  import BackDeselectContactButton from './BackDeselectContactButton.svelte'
+
+  import {LookupNameAsync} from '../data-interfaces/arweave/applications/arweaveId'
+
+  export let mode;
+  export let selected;
+
+  export let title;
+</script>
+
+<NavBar
+      backNavItem={BackDeselectContactButton}
+      title={title} />
